68b5e68636de83e4a76323ef4ed103551c7c0a7e
jackyk <jackykimani13@gmail.com>
2017-03-02 09:34:40 +0300

    first step

---
+++ plugin/calculator.php
@@ -0,0 +1,9 @@
+<?php
+/*Plugin Name: Mortgage Calculator
+  Plugin URI: https://github.com/tunapanda/Mortgage_calculator/
+  GitHub Plugin URI: https://github.com/tunapanda/Mortgage_calculator/
+  Description: Send and get declarations to filter reports, for example
+	Author:Tunapanda
+	Version:1.0
+	Author URI:
+	*/
