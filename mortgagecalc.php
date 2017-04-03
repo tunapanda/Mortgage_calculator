12314cb724e8cd48f9f204e29fc606eb0fc0f749
jackyk <jackykimani13@gmail.com>
2017-04-03 11:46:46 +0300

    files added

---
+++ mortgagecalc.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Property Check
+Plugin URI: https://github.com/tunapanda/Mortgage_calculator
+GitHub Plugin URI: https://github.com/tunapanda/Mortgage_calculator
+Description: A demo that shows how to split the code into Model, View and Controller.
+Version: 0.0.1
+*/
---
+++ mortgagecalc.tpl.php
@@ -0,0 +1,3 @@
+<?php
+require "mortgagecalc.php";
+echo "This is a test";  
